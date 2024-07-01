0ad14b2f1ac9d294bad9bb3ac101a99b1be55728
parlough <parlough@users.noreply.github.com>
2024-07-01 07:14:36 +0000

    deploy: 48bd40d1292edad42481968d2e0e916e492ead6f

--- web/material_3_demo/flutter_bootstrap.js
+++ web/material_3_demo/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "775702289"
+    serviceWorkerVersion: "4281893670"
   }
 });
